8b2554dbff4002fdca0809099aa81109f513b5e7
Daniel Pötzinger <poetzinger@aoemedia.de>
2014-12-20 14:34:41 +0100

    add common functions - we will need this for java deployment etc

---
+++ _utils.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -ex
+
+
+####
+# Download function to download a package
+# @param package_url
+# @param targetDir
+#
+# Global Variables read: USERNAME, PASSWORD, AWSCLIPROFILE
+#
+####
+function download  {
+    local package_url=$1
+    local targetDir=$2
+
+    if [ -f "${package_url}" ] ; then
+        cp "${package_url}" "${targetDir}/package.tar.gz" || { echo "Error while copying base package to ${targetDir}" ; exit 1; }
+    elif [[ "${package_url}" =~ ^https?:// ]] ; then
+        if [ ! -z "${USERNAME}" ] && [ ! -z "${PASSWORD}" ] ; then
+            echo "Using username ${USERNAME} and given password to download"
+            # TODO - Escape wget credentials parameter proberly
+            CREDENTIALS="--user=${USERNAME} --password=${PASSWORD}"
+        fi
+        echo "Downloading package via http"
+        wget --no-check-certificate --auth-no-challenge ${CREDENTIALS} "${package_url}" -O "${targetDir}/package.tar.gz" || { echo "Error while downloading base package from http" ; exit 1; }
+
+    elif [[ "${package_url}" =~ ^s3:// ]] ; then
+        echo "Downloading package via S3"
+        if [ -z "AWSCLIPROFILE" ]; then
+             echo "Missing Variable AWSCLIPROFILE"
+            exit 1
+        fi
+        aws --profile ${AWSCLIPROFILE} s3 cp "${package_url}" "${targetDir}/package.tar.gz" || { echo "Error while downloading base package from S3" ; exit 1; }
+
+    fi
+}
