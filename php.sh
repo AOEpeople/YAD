0fe03290c9d1b683a4600a0c86b02660d5242ea6
Thomas Neumann <thomas.neumann@aoe.com>
2016-09-12 09:37:00 +0200

    [BUG] Run cleanup trap also on process kill

--- php.sh
+++ php.sh
@@ -57,7 +57,7 @@ function cleanup {
     rm -rf "${TMPDIR}"
     if [ -f "${YAD_RELEASE_FOLDER}/INSTALLING.lock" ]; then rm ${YAD_RELEASE_FOLDER}/INSTALLING.lock; fi
 }
-trap cleanup EXIT
+trap cleanup
 
 # Call download function
 download $YAD_PACKAGE $TMPDIR 'package.tar.gz'
